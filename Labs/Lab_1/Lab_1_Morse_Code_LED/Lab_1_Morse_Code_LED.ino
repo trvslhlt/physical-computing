50827aafca9dc5f3fd5e5bbddc4e2d07d960aae0
trvslhlt <trvslhlt@gmail.com>
2016-09-02 11:59:48 -0400

    Flashing morse code on the built-in LED

--- Labs/Lab_1/Lab_1_Morse_Code_LED/Lab_1_Morse_Code_LED.ino
+++ Labs/Lab_1/Lab_1_Morse_Code_LED/Lab_1_Morse_Code_LED.ino
@@ -2,27 +2,23 @@
  * This sketch translates numerals 0-9 into Morse Code flashes on the Arduino Fio's built-in LED
  */
 
-String numeralToFlash = "";
+char numeralToFlash = '\0';
 
 void setup() {
   Serial.begin(9600); // baud rate
-  numeralToFlash.reserve(1); // only storing a single character
 }
 
 void loop() {
-  if (numeralToFlash.length() == 0) { // if we have a number to flash
+  if (numeralToFlash == '\0') { // if we have a number to flash
     return;  
+  } else {
+    flashNumeral();
+    numeralToFlash = '\0';
   }
-  Serial.println(numeralToFlash);
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);              // wait for a second
-  numeralToFlash = "";
 }
 
 void serialEvent() {
-  char readChar = "";
+  char readChar = '\0';
   int readCharCount = 0;
   
   while (Serial.available()) {
@@ -30,10 +26,68 @@ void serialEvent() {
     readCharCount += 1;
   }
   
-  if (readCharCount == 1 && isDigit(readChar) && !numeralToFlash.length()) {
+  if (readCharCount == 1 && isDigit(readChar) && numeralToFlash == '\0') {
     numeralToFlash = readChar;
   } else {
-    numeralToFlash = "";
+    numeralToFlash = '\0';
   }
 }
 
+void flashNumeral() {
+  Serial.println(numeralToFlash);
+  int patternLength = 10;
+  int pattern[patternLength];
+  morsePatternForNumeral(numeralToFlash, pattern);
+  for (int i = 0; i < patternLength; i++) {
+    if (i % 2) {
+      digitalWrite(13, LOW);
+    } else {
+      digitalWrite(13, HIGH);  
+    }
+    delay(pattern[i]);
+  }
+}
+
+void morsePatternForNumeral(char numeral, int *pattern) {
+  int dot = 100;
+  int dash = 500;
+  int rest = 200;
+
+  if (numeral == '0') {
+    int morsePattern[10] = {dash, rest, dash, rest, dash, rest, dash, rest, dash, rest};
+    assignElements(10, pattern, morsePattern);
+  } else if (numeral == '1') {
+    int morsePattern[10] = {dot, rest, dash, rest, dash, rest, dash, rest, dash, rest};
+    assignElements(10, pattern, morsePattern);
+  } else if (numeral == '2') {
+    int morsePattern[10] = {dot, rest, dot, rest, dash, rest, dash, rest, dash, rest};
+    assignElements(10, pattern, morsePattern);
+  } else if (numeral == '3') {
+    int morsePattern[10] = {dot, rest, dot, rest, dot, rest, dash, rest, dash, rest};
+    assignElements(10, pattern, morsePattern);
+  } else if (numeral == '4') {
+    int morsePattern[10] = {dot, rest, dot, rest, dot, rest, dot, rest, dash, rest};
+    assignElements(10, pattern, morsePattern);
+  } else if (numeral == '5') {
+    int morsePattern[10] = {dot, rest, dot, rest, dot, rest, dot, rest, dot, rest};
+    assignElements(10, pattern, morsePattern);
+  } else if (numeral == '6') {
+    int morsePattern[10] = {dash, rest, dot, rest, dot, rest, dot, rest, dot, rest};
+    assignElements(10, pattern, morsePattern);
+  } else if (numeral == '7') {
+    int morsePattern[10] = {dash, rest, dash, rest, dot, rest, dot, rest, dot, rest};
+    assignElements(10, pattern, morsePattern);
+  } else if (numeral == '8') {
+    int morsePattern[10] = {dash, rest, dash, rest, dash, rest, dot, rest, dot, rest};
+    assignElements(10, pattern, morsePattern);
+  } else if (numeral == '9') {
+    int morsePattern[10] = {dash, rest, dash, rest, dash, rest, dash, rest, dot, rest};
+    assignElements(10, pattern, morsePattern);
+  }
+}
+
+void assignElements(int len, int *target, int *donor) {
+    for (int i = 0; i < len; i++) {
+      target[i] = donor[i];
+    }
+}
