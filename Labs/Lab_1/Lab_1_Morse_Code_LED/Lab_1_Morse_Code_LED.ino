ad2b5c58643be0754ac493c30212ad1c4842a677
trvslhlt <trvslhlt@gmail.com>
2016-09-02 10:38:27 -0400

    Flash LED for single numeral serial input only
    - not blinking Morse code

--- Labs/Lab_1/Lab_1_Morse_Code_LED/Lab_1_Morse_Code_LED.ino
+++ Labs/Lab_1/Lab_1_Morse_Code_LED/Lab_1_Morse_Code_LED.ino
@@ -1,9 +1,39 @@
-void setup() {
-  // put your setup code here, to run once:
+/*
+ * This sketch translates numerals 0-9 into Morse Code flashes on the Arduino Fio's built-in LED
+ */
 
+String numeralToFlash = "";
+
+void setup() {
+  Serial.begin(9600); // baud rate
+  numeralToFlash.reserve(1); // only storing a single character
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  if (numeralToFlash.length() == 0) { // if we have a number to flash
+    return;  
+  }
+  Serial.println(numeralToFlash);
+  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
+  numeralToFlash = "";
 }
+
+void serialEvent() {
+  char readChar = "";
+  int readCharCount = 0;
+  
+  while (Serial.available()) {
+    readChar = (char)Serial.read();
+    readCharCount += 1;
+  }
+  
+  if (readCharCount == 1 && isDigit(readChar) && !numeralToFlash.length()) {
+    numeralToFlash = readChar;
+  } else {
+    numeralToFlash = "";
+  }
+}
+
