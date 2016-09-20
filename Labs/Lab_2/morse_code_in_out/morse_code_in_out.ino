965ca72d493625eead946e2b9aadd18613d09c23
trvslhlt <trvslhlt@gmail.com>
2016-09-20 19:25:54 -0400

    Shift register outputting to LED grid

--- Labs/Lab_2/morse_code_in_out/morse_code_in_out.ino
+++ Labs/Lab_2/morse_code_in_out/morse_code_in_out.ino
@@ -1,51 +1,24 @@
-//https://www.arduino.cc/en/Tutorial/ShftOut23
-
-int latchPin = 8;
-int clockPin = 12;
-int dataPin = 11;
-
-void setup() {
-  // put your setup code here, to run once:
+int latchPin = 5;
+int clockPin = 6;
+int dataPin = 4;
+byte leds = 1;
+ 
+void setup() 
+{
   pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);  
   pinMode(clockPin, OUTPUT);
-  pinMode(dataPin, OUTPUT);
 }
 
 void loop() {
-  delay(1000);
-  digitalWrite(latchPin, LOW);
-  for (int i = 0; i < 8; i++) {
-    digitalWrite(clockPin, LOW);
-    digitalWrite(dataPin, HIGH);
-    digitalWrite(clockPin, HIGH); 
+  leds = B11111111;
+  for (int i = 1; i < 8; i++) {
+    bitClear(leds, i);
+    bitSet(leds, i - 1);
+    digitalWrite(latchPin, LOW);
+    shiftOut(dataPin, clockPin, LSBFIRST, leds);
+    digitalWrite(latchPin, HIGH);
+    delay(500);
   }
-  digitalWrite(latchPin, HIGH);
 }
 
-//  byte data = B0001111;
-//  digitalWrite(latchPin, LOW);
-//  delay(500);
-//  shiftOut(data);
-//  digitalWrite(latchPin, HIGH);
-//  delay(1000);
-//}
-
-void shiftOut(byte myDataOut) {
-  
-  digitalWrite(clockPin, LOW);
-  digitalWrite(dataPin, LOW);
-  int pinState = LOW;
-  
-  for (int i = 7; i >= 0; i--) {
-    if ( myDataOut & (1<<i) ) {
-      pinState = HIGH;
-    } else {  
-      pinState = LOW;
-    }
-    digitalWrite(clockPin, LOW);
-    digitalWrite(dataPin, pinState);
-    digitalWrite(clockPin, HIGH);
-    digitalWrite(dataPin, LOW); // reset to prevent "bleed through"
-  }
-  digitalWrite(clockPin, LOW);  
-}
