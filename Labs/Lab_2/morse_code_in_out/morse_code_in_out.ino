5ea0e0e3052631fa3111c9d7b9418399ae4e9999
trvslhlt <trvslhlt@gmail.com>
2016-09-21 16:01:16 -0400

    Debouncing button press

--- Labs/Lab_2/morse_code_in_out/morse_code_in_out.ino
+++ Labs/Lab_2/morse_code_in_out/morse_code_in_out.ino
@@ -1,24 +1,63 @@
+//https://www.arduino.cc/en/Tutorial/Debounce
+
+
 int latchPin = 5;
 int clockPin = 6;
 int dataPin = 4;
-byte leds = 1;
+int buttonPin = 13;
+bool buttonIntent = false; // not pressed
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 0;
+
  
-void setup() 
-{
+void setup() {
   pinMode(latchPin, OUTPUT);
   pinMode(dataPin, OUTPUT);  
   pinMode(clockPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  Serial.begin(9600);
 }
 
-void loop() {
-  leds = B11111111;
-  for (int i = 1; i < 8; i++) {
-    bitClear(leds, i);
-    bitSet(leds, i - 1);
-    digitalWrite(latchPin, LOW);
-    shiftOut(dataPin, clockPin, LSBFIRST, leds);
+void loop() { 
+  determineButtonIntent();
+  
+}
+
+void animateLEDs() {
+  for (int i = 0; i < 7; i++) {
+    digitalWrite(latchPin, LOW);    
+    digitalWrite(clockPin, LOW);
+    digitalWrite(dataPin, LOW);
+    digitalWrite(dataPin, (i == 0) ? (HIGH) : (LOW));
+    digitalWrite(clockPin, HIGH);
     digitalWrite(latchPin, HIGH);
     delay(500);
+  }
+}
+
+// debounce button action
+void determineButtonIntent() {
+  bool buttonPressed = digitalRead(buttonPin);
+  if (buttonPressed != buttonIntent) {
+    lastDebounceTime = millis();
+  }
+
+  // if we are confident we know the state
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    
+    if (buttonIntent != buttonPressed) {
+      changeButtonIntent(buttonPressed);
+    }
+    
+  }
+}
+
+void changeButtonIntent(bool pressed) {
+  buttonIntent = pressed;
+  if (buttonIntent) {
+    Serial.print("pressed"); 
+  } else {
+    Serial.print("-------");  
   }
 }
 
