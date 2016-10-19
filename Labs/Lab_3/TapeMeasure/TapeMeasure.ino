8948ea4b82e8520e182dc7e313fbde5b366db8e7
trvslhlt <trvslhlt@gmail.com>
2016-10-18 21:09:29 -0400

    Sends distance in mm separated by new lines

--- Labs/Lab_3/TapeMeasure/TapeMeasure.ino
+++ Labs/Lab_3/TapeMeasure/TapeMeasure.ino
@@ -1,27 +1,86 @@
 #include <SoftwareSerial.h>
 
+
 int rxPin = 0; // specific to the FIO
 int txPin = 1; // specific to the FIO
-int ledPin = 13;
+int pwmPin = 13;
+int pwmValue = 0;
+char newLine = '\n';
 SoftwareSerial xbee(rxPin, txPin);
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
+  pinMode(pwmPin, INPUT);
   pinMode(rxPin, INPUT);
   pinMode(txPin, OUTPUT);
   xbee.begin(9600);
 }
 
 void loop() {
-  while (xbee.available()) {
-    char c = xbee.read();
-    indicateData();
-  }
+  readDistance();
+  transmitDistance();
 }
 
-void indicateData() {
-  digitalWrite(ledPin, HIGH);
-  delay(1000);
-  digitalWrite(ledPin, LOW);
-  delay(100);  
+void readDistance() {
+  pwmValue = pulseIn(pwmPin, HIGH);  
+}
+
+void transmitDistance() {
+  xbee.print(pwmValue);  
+  xbee.print(newLine);
+}
+
+
+
+
+
+
+
+
+
+
+
+//void readDistance() {
+//int pwmPin = 13;
+//int pwmValue;
+//
+//void setup() {
+//  pinMode(pwmPin, INPUT);
+//  Serial.begin(9600);
+//}
+//
+//void loop() {
+//  pwmValue = pulseIn(pwmPin, HIGH);
+//  Serial.println(pwmValue);
+//}  
+//}
+
+
+
+
+void readFromXBee() {
+//int rxPin = 0; // specific to the FIO
+//int txPin = 1; // specific to the FIO
+//int ledPin = 13;
+//SoftwareSerial xbee(rxPin, txPin);
+//
+//void setup() {
+//  pinMode(ledPin, OUTPUT);
+//  pinMode(rxPin, INPUT);
+//  pinMode(txPin, OUTPUT);
+//  xbee.begin(9600);
+//}
+//
+//void loop() {
+//  while (xbee.available()) {
+//    char c = xbee.read();
+//    indicateData();
+//  }
+//}
+//
+//void indicateData() {
+//  digitalWrite(ledPin, HIGH);
+//  delay(1000);
+//  digitalWrite(ledPin, LOW);
+//  delay(100);  
+//}
 }
